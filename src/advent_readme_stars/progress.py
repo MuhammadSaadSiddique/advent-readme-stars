ff833f623367ba963e5771b9aa478d97fc5fd431
Muhammad Saad Siddique <26270646+MuhammadSaadSiddique@users.noreply.github.com>
2023-12-05 00:47:50 +0500

    Update progress.py

--- src/advent_readme_stars/progress.py
+++ src/advent_readme_stars/progress.py
@@ -3,7 +3,7 @@ from typing import Generator
 
 import requests
 
-from advent_readme_stars.constants import SESSION_COOKIE, STARS_ENDPOINT, USER_ID
+from advent_readme_stars.constants import SESSION_COOKIE,  USER_ID, YEAR
 
 
 @dataclass(frozen=True, eq=True)
@@ -14,17 +14,25 @@ class DayProgress:
 
 
 def get_progress() -> Generator[DayProgress, None, None]:
-    res = requests.get(STARS_ENDPOINT, cookies={"session": SESSION_COOKIE})
-    res.raise_for_status()
-
-    leaderboard_info = res.json()
-
-    stars = leaderboard_info["members"][USER_ID]["completion_day_level"]
-
-    for day, parts in stars.items():
-        completed = parts.keys()
-        yield DayProgress(
-            day=int(day),
-            part_1="1" in completed,
-            part_2="2" in completed,
-        )
+    for y in YEAR.split(","):
+        STARS_ENDPOINT = f"{ADVENT_URL}/{y}/leaderboard/private/view/{LEADERBOARD_ID}.json"
+        res = requests.get(STARS_ENDPOINT, cookies={"session": SESSION_COOKIE})
+        res.raise_for_status()
+    
+        leaderboard_info = res.json()
+        
+        file = open(f"{y}.py", 'w')
+        file.write(leaderboard_info)
+        file.close()
+        for member,detail in leaderboard_info["members"].items():
+            stars = member["completion_day_level"]
+        
+            for day, parts in stars.items():
+                completed = parts.keys()
+                yield DayProgress(
+                    member["name"],
+                    member["local_score"]
+                    day=int(day),
+                    part_1="1" in completed,
+                    part_2="2" in completed,
+                )
