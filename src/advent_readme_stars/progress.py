e626dcbe04cf5da8b0653d4fd66367f70607d134
Muhammad Saad Siddique <26270646+MuhammadSaadSiddique@users.noreply.github.com>
2023-12-05 17:03:09 +0500

    Update progress.py to fetch all users

--- src/advent_readme_stars/progress.py
+++ src/advent_readme_stars/progress.py
@@ -8,9 +8,13 @@ from advent_readme_stars.constants import SESSION_COOKIE,  USER_ID, YEAR, ADVENT
 
 @dataclass(frozen=True, eq=True)
 class DayProgress:
-    day: int
-    part_1: bool
-    part_2: bool
+    name: str
+    score: int
+    day:int
+    part_1:bool
+    part_1ts:int
+    part_2:bool
+    part_2ts:int
 
 
 def get_progress(y) -> Generator[DayProgress, None, None]:
@@ -20,19 +24,26 @@ def get_progress(y) -> Generator[DayProgress, None, None]:
 
     leaderboard_info = res.json()
     
-    with open(f"{y}.py", 'w') as f:
+    with open(f"{y}.json", 'w') as f:
         json.dump(leaderboard_info, f)
+    members={}
     for member,detail in leaderboard_info["members"].items():
-        stars = member["completion_day_level"]
-    
+        # print(member,detail)
+        
+        stars = detail["completion_day_level"]
+        
         for day, parts in stars.items():
             completed = parts.keys()
-            yield DayProgress(
-                name=member["name"],
-                score=member["local_score"],
+            
+            dp= DayProgress(
+                name=detail["name"],
+                score=detail["local_score"],
                 day=int(day),
                 part_1="1" in completed,
-                part_1ts=day["1"]["get_star_ts"] if "1" in completed else -1 ,
+                part_1ts=parts["1"]["get_star_ts"] if "1" in completed else -1 ,
                 part_2="2" in completed,
-                part_2ts=day["2"]["get_star_ts"] if "2" in completed else -1,
+                part_2ts=parts["2"]["get_star_ts"] if "2" in completed else -1,
             )
+            members[member]=dp
+            # print(dp)
+            yield dp
